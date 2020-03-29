3cadb3817963f5d38aefb58c511a84406f50bf39
Estella Mystagic <Estella@Mystagic.com>
2020-03-29 10:41:06 -0600

    update for csv data

--- ccovid.php
+++ ccovid.php
@@ -1,5 +1,4 @@
 <?php
-
 /*******************************************************************************************************/
 function getpage() {
   $memcache = new Memcached();
@@ -8,79 +7,74 @@ function getpage() {
   if ($page == "") {
     $page = $memcache->get('CCOVID19');
     if ($page == "") {
-      $page = curl_download('https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html');
-      $memcache->set('CCOVID19', $page, time() + 60);
+      $page = curl_download('https://health-infobase.canada.ca/src/data/summary_current.csv');
+      $memcache->set('CCOVID19', $page, time() + 900);
     }
-    $page = process_html($page,0);
-    $memcache->set('CCOVID19OUT', $page, time() + 60);
+    $page = process_page($page);
+    if ($page) { $memcache->set('CCOVID19OUT', $page, time() + 900); }
     return $page;
   }
   return $page;
 }
 /*******************************************************************************************************/
-function getjson() {
-  $memcache = new Memcached();
-  $memcache->addServer('127.0.0.1', 11211);
-  $page = $memcache->get('CCOVID19JOUT');
-  if ($page == "") {
-    $page = $memcache->get('CCOVID19');
-    if ($page == "") {
-      $page = curl_download('https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html');
-      $memcache->set('CCOVID19', $page, time() + 60);
-    }
-    $page = process_html($page,1);
-    $memcache->set('CCOVID19JOUT', $page, time() + 60);
-    return $page;
-  }
-  return $page;
-}
-/*******************************************************************************************************/
-function tdrows($elements,$type = 0) {
-  $output = array();
-  foreach ($elements as $element) {
-    $output[] = str_pad(trim(str_replace(array(",","Total:"),array("","GLOBAL"),$element->nodeValue)), 1, "-", STR_PAD_LEFT);
-  }
-  $output2 = array();
-  if ($type == 1) {
-  $country = $output[1];
-  $output2[$country][] = $output[3]; // confirmed cases
-  $output2[$country][] = $output[5]; // probable cases
-  $output2[$country][] = $output[7]; // deaths
-  } else {
-  $output2[] = $output[1]."_START"; // province,territory
-  $output2[] = $output[3]; // confirmed cases
-  $output2[] = $output[5]; // probable cases
-  $output2[] = $output[7]; // deaths
-  $output2[] = $output[1]."_END";
-  }
-  return $output2;
-}
-/*******************************************************************************************************/
-function process_html($data,$type = 0) {
-  if (preg_match('/<table class="table table-striped table-bordered">(.*)<div class="mwsgeneric-base-html parbase section">/si', $data, $regs)) {
-    if (preg_match('#<table class="table table-striped table-bordered">(.*)</table>#si', $regs[0], $contents)) {
-      $contents = $contents[0];
-      $DOM = new DOMDocument;
-      $DOM->loadHTML($contents);
-      $items = $DOM->getElementsByTagName('tr');
-      $output = array();
-      foreach ($items as $node) {
-        $output[] = tdrows($node->childNodes,$type);
-      }
-      array_shift($output); // del header
-//      array_pop($output); // del total footer
-      $body = "";
-      if ($type == 1) {
-        $body = json_encode($output);
-      } else {
-        foreach ($output as $country) {
-          $body .= implode(",",$country) . "\n";
-        }
-      }
-      return $body;
-    }
+function process_page($data) {
+  $attempts = 0;
+  $cdate = date('d-m-Y');
+  $output = process_csv($data,$cdate);
+  if ($output) { return $output; }
+  for($attempts = 1; $attempts < 7; $attempts++) {
+    $output = process_csv($data,date('d-m-Y',strtotime("-${attempts} day",time())));
+    if ($output) { return $output; }
   }
   return 0;
+}
+/*******************************************************************************************************/
+function process_csv($data,$cdate) {
+  $data2 = explode("\n",trim($data));
+  $output = array();
+  foreach ($data2 as $row) {
+    $row = explode(",",$row);
+    $row = array_pad($row,9,0);
+    list($pruid,$prname,$prnameFR,$xdate,$numconf,$numprob,$numdeaths,$numtotal,$numtoday) = $row;
+    if ($xdate == $cdate) {
+      $output[$pruid]['name'] = $prname;
+
+      if (isset($output[$pruid]['confirmed'])) {
+        $output[$pruid]['confirmed'] += (int)$numconf;
+      } else {
+        $output[$pruid]['confirmed'] = (int)$numconf;
+      }
+
+      if (isset($output[$pruid]['probable'])) {
+        $output[$pruid]['probable'] += (int)$numprob;
+      } else {
+        $output[$pruid]['probable'] = (int)$numprob;
+      }
+
+      if (isset($output[$pruid]['deaths'])) {
+        $output[$pruid]['deaths'] += (int)$numdeaths;
+      } else {
+        $output[$pruid]['deaths'] = (int)$numdeaths;
+      }
+    }
+  }
+  $output2 = array();
+  if (count($output) > 0) {
+    foreach($output as $k => $v) {
+      $vdata = array();
+      $vdata[] = $output[$k]['name']."_START"; // province,territory
+      $vdata[] = $output[$k]['confirmed']; // confirmed cases
+      $vdata[] = $output[$k]['probable']; // probable cases
+      $vdata[] = $output[$k]['deaths']; // deaths
+      $vdata[] = $output[$k]['name']."_END";
+      $output2[] = implode(",",$vdata);
+    }
+  }
+  if (count($output2) > 0) {
+    return implode("\n",$output2);
+  } else {
+    return 0;
+  }
 }
 /*******************************************************************************************************/
 function curl_download($url) {
@@ -99,15 +93,8 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-$json_out = 0;
-$json_out = (int) (isset($_GET['json']) ? 1 : 0);
-if ($json_out) {
-header("Content-Type: application/json");
-echo getjson();
-} else {
 header("Content-Type: text/plain");
 echo trim(getpage());
-}
 /*******************************************************************************************************/
 // EOF
 
